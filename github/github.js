78eeced13022a68e2c03e8c7fd59d72761f70e43
George Snelling <georgesn@gmail.com>
2019-11-08 18:02:25 -0800

    interim commit, not working

--- github/github.js
+++ github/github.js
@@ -5,6 +5,11 @@
 
 // https://octokit.github.io/rest.js
 var Octokit = require("@octokit/rest")
+var _ = require("lodash")
+
+
+const owner = 'georgesnelling'
+const repo = 'stuff'
 
 
 // soon to be translated into 90 languages
@@ -26,12 +31,39 @@ const gh = new Octokit({ auth: "token " + token })
 // make sure we're singed in properly
 gh.users
   .getAuthenticated()
-  .then(user => { console.log("user:", user) })
+  .then(res => {
+    if (res.data && res.data.id) {
+      console.log("user: ", res.data.login, "id: ", res.data.id)
+    } else {
+      die('ERROR: Authentication failed')
+    }
+  })
+  .catch(err => { die(err) })
+
+
+async function listIssues(ops) {
+  _.merge(ops, { owner: owner, repo: repo })
+  const issueCursor = gh.issues.listForRepo.endpoint.merge(ops)
+
+  gh.paginate(issueCursor)
+    .then(issues => { console.log(issues); return issues })
+    .catch(err => die(err))
+}
+
+
+listIssues({state: 'open'})
+  .then(issues => {
+    console.log('ran')
+    // console.log("Count of issues: ", issues.length)
+    // issues.forEach(issue => {
+    //  console.log("issue", issue)
+    // })
+  })
   .catch(err => { die(err) })
 
 
 // don't panic, just die
 function die(msg) {
-  console.error(msg)
+  console.error(msg || 'Error')
   process.exit(1)
 }
