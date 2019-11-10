4aa0f92e05ab8cde6c8f4e2c83cc6e3cc0bc5d9f
George Snelling <georgesn@gmail.com>
2019-11-09 18:25:00 -0800

    now with prettier promises

--- github/github.js
+++ github/github.js
@@ -5,14 +5,15 @@
 // everyone's favorite
 const _ = require("lodash")
 
-// https://octokit.github.io/rest.js
-const Octokit = require("@octokit/rest")
-
 
 // only one way to auth: Github personal access tokens as an env var
 // args would be nice
 const token = process.env.GITHUB_TOKEN
 if (!token) reject(strs.missing_token)
+
+
+// https://octokit.github.io/rest.js
+const Octokit = require("@octokit/rest")
 const ok = new Octokit({ auth: "token " + token })
 
 
@@ -20,41 +21,20 @@ const ok = new Octokit({ auth: "token " + token })
 const owner = 'georgesnelling'
 const repo = 'stuff'
 
-// execute main
-main()
-
-// main
-function main() {
-
-  authenticate()
-    .then(res => {
-      console.log("user: ", {login: res.data.login, id: res.data.id})
-      listIssues({state: 'open'})
-        .then(issues => {
-          console.log("Count of issues: ", issues.length)
-          issues.forEach(issue => {
-            console.log("issue", issue)
-          })
-        })
-    })
-    .catch(err => { die(err) })
-}
-
 
 // make sure we're singed in properly
 async function authenticate() {
 
-  return new Promise(function(resolve, reject) {
-    ok.users.getAuthenticated()
-      .then(response => {
-        if (response.data && response.data.id) {
-          resolve(response)
-        } else {
-          reject(new Error('ERROR: Authentication failed', response))
-        }
-      })
-      .catch(err => { reject(err) })
-  })
+  return await ok.users.getAuthenticated()
+    .then(resp => {
+      // missing res.data.id means not authenticated
+      if (resp.data && resp.data.id) {
+        return(resp.data)
+      }
+      else {
+        throw new Error('ERROR: Authentication failed', resp)
+      }
+    })
 }
 
 
@@ -63,27 +43,38 @@ async function listIssues(ops) {
 
   _.merge(ops, { owner: owner, repo: repo })
 
-  // this is a weird command due to the design of Oktokit
+  // this is a weird sync command due to the design of Oktokit
   const issueCursor = ok.issues.listForRepo.endpoint.merge(ops)
 
-  // returns an arry of issues or an error, max number of issues
-  // returned is up to github: 3,000 as of Nov 2019
-  return new Promise(function(resolve, reject) {
-    ok.paginate(issueCursor)
-      .then(issues => { resolve(issues) })
-      .catch(err => { reject(err) })
-  })
+  return ok.paginate(issueCursor)
 }
-
 
 // soon to be translated into 90 languages
 const strs = {
   missing_token: "env var GITHUB_TOKEN must be defined"
 }
 
+// very lazy
+let log = console.log
 
 // don't panic, just die
-var die = function(err, code) {
+let die = function(err, code) {
   console.error(err || 'Error')
   process.exit(code || 1)
 }
+
+// main
+function main() {
+
+  authenticate()
+    .then(data => { log("user: ", {login: data.login, id: data.id}) })
+    .then(_ => listIssues({state: 'open'}))
+    .then(issues => {
+      log("Count of issues: ", issues.length)
+      issues.forEach(issue => { log("issue: ", issue) })
+    })
+    .catch(err => { die(err) })
+}
+
+// execute main
+main()
